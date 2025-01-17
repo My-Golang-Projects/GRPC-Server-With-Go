6209caf1c4eb09e727ef6a2a8eef63c8c645bd4a
shrish <adhikari.shrish1@gmail.com>
2025-01-17 10:27:05 +0545

    Refactor the net listener

--- main.go
+++ main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"net"
 
+	invoicer "github.com/My-Golang-Projects/GRPC-Server-With-Go/invoicer/bufs/v1"
 	"github.com/prometheus/common/log"
 )
 
+type Server struct {
+	listenAddr string
+	ln         net.Listener
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+
+	s.ln = ln
+
+	return nil
+}
+
 func main() {
-	lis, err := net.Listen("tcp", ":8080")
+	err := NewServer(":8080")
 	if err != nil {
 		log.Fatalf("cannot create listener: %s", err)
 	}
+
+	// invoicer.New
 }
